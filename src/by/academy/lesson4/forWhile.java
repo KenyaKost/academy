f103ed1b1644291fee4b298df17913919a7a95a2
User <dfbth@fghj.ty>
2023-11-13 20:51:07 +0300

    13.11.2023 lesson4 for while

--- src/by/academy/lesson4/forWhile.java
+++ src/by/academy/lesson4/forWhile.java
@@ -18,15 +18,23 @@ public class forWhile {
 //
 //        }
 //        System.out.println("Сумма чисел: " + sum);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("программа для вычисления произведения чисел от 1 до n");
-        System.out.println("Введите n: ");
-        int sum = 0;
-        int enter = sc.nextInt();
-        for (int i = 1; i < enter; i++) {
-            sum = i * i;
-
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("программа для вычисления произведения чисел от 1 до n");
+//        System.out.println("Введите n: ");
+//        int sum = 0;
+//        int enter = sc.nextInt();
+//        for (int i = 1; i < enter; i++) {
+//            sum = i * i;
+//
+//        }
+//        System.out.println("произведение чисел: " + sum);
+    for(int i = 0; i <= 100; i++){
+        if(i % 13 ==0){
+            System.out.print(""); // Таким образом я пропускаю все числа которые кратны 13
+            continue;
         }
-        System.out.println("произведение чисел: " + sum);
+        System.out.println(i); // обязательно в конце
+    }
+
     }
 }
