2cea143439eef12d7a1675a940ee15ba2711f0b6
User <dfbth@fghj.ty>
2023-11-13 20:37:35 +0300

    13.11.2023 lesson4 for while

--- src/by/academy/lesson4/forWhile.java
+++ src/by/academy/lesson4/forWhile.java
@@ -1,9 +1,32 @@
 package by.academy.lesson4;
 
+import java.util.Scanner;
+
 public class forWhile {
     public static void main(String[] args) {
-        for(int i = 10; i <= 20; i++){
-            System.out.println("Квадрат чисел: " + i*i);
+//        for(int i = 10; i <= 20; i++){
+//            System.out.println("Квадрат чисел: " + i*i);
+//        }
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("программа для вычисления суммы чисел от 1 до n");
+//        System.out.println("Введите n: ");
+//        int enter = sc.nextInt();
+//        int sum = 0;
+//        for(int i = 1; i < enter; i++){
+//            sum = sum + i;
+//
+//        }
+//        System.out.println("Сумма чисел: " + sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("программа для вычисления произведения чисел от 1 до n");
+        System.out.println("Введите n: ");
+        int sum = 0;
+        int enter = sc.nextInt();
+        for (int i = 1; i < enter; i++) {
+            sum = i * i;
+
         }
+        System.out.println("произведение чисел: " + sum);
     }
 }
