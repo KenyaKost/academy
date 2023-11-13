e7da047d29a1deeca27609d053cfdd370eb35147
User <dfbth@fghj.ty>
2023-11-13 20:05:36 +0300

    13.11.2023 lesson4 for while

---
+++ src/by/academy/lesson4/forWhile.java
@@ -0,0 +1,9 @@
+package by.academy.lesson4;
+
+public class forWhile {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 20; i++){
+            System.out.println("Квадрат чисел: " + i*i);
+        }
+    }
+}
