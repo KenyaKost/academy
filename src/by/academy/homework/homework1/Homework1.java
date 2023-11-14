d5549ab2415524f3798b3ecdabbbd7a1663c8574
KenyaKost <nik.kostylenko@proton.me>
2023-11-14 19:37:40 +0300

    Задание 1 Ввести с консоли сумму покупки в магазине и возраст покупателя, в зависимости от суммы вывести на экран ФИНАЛЬНУЮ ЦЕНУ с учетом скидки.
    	Скидки:
    	1) Сумма до 100 рублей -> 5%
    	2) Сумма от 100 рублей включая до 200 рублей не включая -> 7%
    	3) Сумма от 200 рублей включая до 300 рублей не включая -> 12%
    	4) Сумма от 300 рублей включая до 400 рублей не включая -> 15%
    	5) Сумма покупки больше 400 рублей включая -> 20%
    
    В случае, если сумма покупки находится в 3 интервале (сумма от 200 рублей включая до 300 рублей не включая)
    и возраст покупателя больше, чем 18 лет -> добавить 4% к скидке (12 + 4), иначе -> отнять 3%.

--- src/by/academy/homework/homework1/Homework1.java
+++ src/by/academy/homework/homework1/Homework1.java
@@ -1,7 +1,28 @@
 package by.academy.homework.homework1;
 
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("ready");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму");
+        double sum = sc.nextInt();
+        System.out.println("Введите возраст");
+        int age = sc.nextInt();
+        if (sum < 100 && sum > 0) {
+
+            System.out.println("Финальная цена: " + (sum - (sum * 5.0 / 100)) + " рублей");
+        } else if (sum >= 100 && sum < 200){
+            System.out.println("Финальная цена: " + (sum - (sum * 7.0 / 100)) + " рублей");
+        } else if( sum >= 200 && sum <300 ){
+            if(age > 18){
+                System.out.println("Финальная цена: " + (sum - (sum * 16.0 / 100)) + " рублей");
+            } else
+                System.out.println("Финальная цена: " + (sum - (sum * 9.0 / 100)) + " рублей");
+
+
+        } else if(sum >= 400){
+            System.out.println("Финальная цена: " + (sum - (sum * 20.0 / 100)) + " рублей");
+        } else System.out.println("Неверный синтаксис");
     }
 }
