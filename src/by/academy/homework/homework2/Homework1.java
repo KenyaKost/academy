60672bd0d89163c0231a36a76e7a47bfd5ba045f
KenyaKost <nik.kostylenko@proton.me>
2023-11-26 17:34:20 +0300

    Задание 1. Ввести две строки с консоли. Определить, является ли одна строка перестановкой символов другой строки. Учитываем регистр. Не использовать сортировку :)

---
+++ src/by/academy/homework/homework2/Homework1.java
@@ -0,0 +1,52 @@
+package by.academy.homework.homework2;
+
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String hello = "hello";
+        String[] letters = new String[word.length()];
+        String[] hello1 = new String[hello.length()];
+        int num1 = 0;
+        int num2 = 0;
+        int result = 0;
+        boolean complete = false;
+        if(word.length() == hello.length()) {
+            for (int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                letters[i] = String.valueOf(c);
+            }
+            for (int i = 0; i < hello.length(); i++) {
+                char c = word.charAt(i);
+                hello1[i] = String.valueOf(c);
+            }
+
+             while(complete == false) {
+                 if (letters[num1].equals(hello1[num2])){
+                     result++;
+                     num2 = 0;
+                     num1++;
+                 }
+                 num2++;
+                 if(num2 == hello1.length){
+                     num2 = 0;
+                     num1++;
+                 }
+                 if (num1 == hello1.length){
+                     complete = true;
+                 }
+             }
+            System.out.println(result);
+             if (result == hello1.length){
+                 System.out.println("Ваше слово является перестановкой слова HELLO");
+             } else System.out.println("Ваше слово НЕ является перестановкой слова HELLO");
+        }
+        }
+
+    }
+
+
+
